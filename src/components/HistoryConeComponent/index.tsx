8e33ecc3d7cdb39ccf01d7191000158a3ae778e9
Maksim <gershunmaks10@gmail.com>
2026-07-03 14:30:35 +0300

    history component

---
+++ src/components/HistoryConeComponent/index.tsx
@@ -0,0 +1,45 @@
+// React Native and Components
+import React from "react"
+import { View, Text, TouchableOpacity } from "react-native"
+import { SvgProps } from "react-native-svg"
+//Libraries
+//Hooks and Redux
+//styles and Icons
+
+export interface HistoryConeComponentProps {
+  title: string
+  subtitle: string
+  Icon: React.FC<SvgProps>   
+}
+
+const HistoryConeComponent = ({
+  title,
+  subtitle,
+  Icon,
+}: HistoryConeComponentProps
+) => {
+  return (
+   <View
+    style={{
+      backgroundColor: "#fff",
+      borderRadius: 10,
+      boxShadow: "0px 2px 8px rgba(0,0,0,0.08)",
+      marginBottom: 20,
+    }}
+    className="flex-row items-center justify-between mx-[24px] py-[13px] pl-[16px] pr-[21px] rounded-[10px] border-[1px] border-white1">
+      <View className="flex-row items-center">
+        <View className="mr-[15px]">
+          <Icon/>
+        </View>
+        <View>
+          <Text className=" text-gray1 text-16">{title}</Text>
+        </View>
+      </View>
+      <View>
+        <Text className=" text-gray1 text-16">{subtitle}</Text>
+      </View>
+    </View>
+  ) 
+}
+
+export default HistoryConeComponent
--- src/screens/StatsScreen/index.tsx
+++ src/screens/StatsScreen/index.tsx
@@ -2,8 +2,9 @@
 import React from "react"
 import { Text, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
-//Libraries
 import HeaderComponent from "components/HeaderComponent"
+import HistoryConeComponent from "components/HistoryConeComponent"
+//Libraries
 //Hooks and Redux
 //styles and Icons
 import GreenConeIcon from "assets/icons/greenCone.svg"
@@ -15,47 +16,16 @@ const StatsScreen: React.FC = () => {
       <HeaderComponent 
          title={"History"}
          />
-      <View
-      style={{
-        backgroundColor: "#fff",
-        borderRadius: 10,
-        boxShadow: "0px 2px 8px rgba(0,0,0,0.08)",
-        marginBottom: 20,
-      }}
-      className="flex-row items-center justify-between mx-[24px] py-[13px] pl-[16px] pr-[21px] rounded-[10px] border-[1px] border-white1">
-        <View className="flex-row items-center">
-          <View className="mr-[15px]">
-            <GreenConeIcon/>
-          </View>
-          <View>
-            <Text className=" text-gray1 text-16">21 March 12:54</Text>
-          </View>
-        </View>
-        <View>
-          <Text className=" text-gray1 text-16">9.0 MMOL</Text>
-        </View>
-      </View>
-      
-      <View
-      style={{
-        backgroundColor: "#fff",
-        borderRadius: 10,
-        boxShadow: "0px 2px 8px rgba(0,0,0,0.08)",
-        marginBottom: 20,
-      }}
-      className="flex-row items-center justify-between mx-[24px] py-[13px] pl-[16px] pr-[21px] rounded-[10px] border-[1px] border-white1">
-        <View className="flex-row items-center">
-          <View className="mr-[15px]">
-            <RedConeIcon/>
-          </View>
-          <View>
-            <Text className=" text-gray1 text-16">21 March 12:54</Text>
-          </View>
-        </View>
-        <View>
-          <Text className=" text-gray1 text-16">9.0 MMOL</Text>
-        </View>
-      </View>
+      <HistoryConeComponent
+         Icon={GreenConeIcon}
+         title={"21 March 12:54"}
+         subtitle={"9.0 MMOL"}
+      />      
+      <HistoryConeComponent
+        Icon={RedConeIcon}
+        title={"21 March 12:54"}
+        subtitle={"9.0 MMOL"}
+      />
     </SafeAreaView>
   )
 }
